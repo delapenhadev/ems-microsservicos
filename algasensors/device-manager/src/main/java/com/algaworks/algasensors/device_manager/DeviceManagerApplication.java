87ea96b58e52add7d2d57be8e591b721ffded5a7
delapenhadev <costaadevaldo53@gmail.com>
2026-02-08 20:33:31 -0300

    Inicio do Projeto

---
+++ algasensors/device-manager/src/main/java/com/algaworks/algasensors/device_manager/DeviceManagerApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algasensors.device_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviceManagerApplication.class, args);
+	}
+
+}
